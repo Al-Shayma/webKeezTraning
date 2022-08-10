b125a751219136dc61cae34d5f412fa332db676a
Al-Shayma <shayma.raheem.97@gmail.com>
2022-08-10 15:19:39 +0200

    Show Toast

--- app/src/main/java/com/example/getage/MainActivity2.kt
+++ app/src/main/java/com/example/getage/MainActivity2.kt
@@ -2,7 +2,9 @@ package com.example.getage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,9 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         findViewById<TextView>(R.id.textView).text = intent.extras?.get("name").toString()
+        val clickNow = findViewById<Button>(R.id.clickNowB);
+        clickNow.setOnClickListener {
+            Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
+        }
     }
 }
